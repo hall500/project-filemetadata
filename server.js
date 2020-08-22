7eaa947eaf280a129caa327f93fc00afbba678b3
repl.it user <replituser@example.com>
2020-08-22 03:28:49 +0000

    Uploading of files using multer

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -18,7 +20,13 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next){
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
